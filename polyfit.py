a089a430aa841771a6bfd5880ef5fa86745e112b
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-04-17 07:04:53 -0700

    Add Jupyter notebook and #%% sections in python file.

--- polyfit.py
+++ polyfit.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(0, 1, 51)
 print(x)
+
 y = x**3 - x + (np.random.randn(len(x)) / 10)
 # y = x**2
 print(y)
 
+#%%
 c, stats = poly.polyfit(x, y, 3, full=True)
 yt = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
 
 # c, stats = P.polyfit(x,y,2,full=True)
 # yt = c[0] + c[1]*x + c[2]*x**2
 
+#%%
 plt.figure(1)
 plt.subplot(211)
 plt.plot(x, y)
