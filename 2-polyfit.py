8e0bfb884a5818aeb72078cbaf78aae14e67c838
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-05-04 19:34:05 -0700

    Number files. Remove #%%.

--- 2-polyfit.py
+++ 2-polyfit.py
@@ -9,18 +9,15 @@ y = x**3 - x + (np.random.randn(len(x)) / 10)
 # y = x**2
 print(y)
 
-#%%
 c, stats = poly.polyfit(x, y, 3, full=True)
 yt = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
 
 # c, stats = P.polyfit(x,y,2,full=True)
 # yt = c[0] + c[1]*x + c[2]*x**2
 
-#%%
 plt.figure(1)
 plt.subplot(211)
 plt.plot(x, y)
-
 plt.subplot(212)
 plt.plot(x, yt)
 plt.show()
