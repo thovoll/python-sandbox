7067d3ea8c6530362a538bf30b6ca0bb113563aa
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-05-04 21:48:05 -0700

    Pandas.

---
+++ 4-pandas.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+# DataFrame is a 2-dimensional labeled data structure with columns of potentially different types.
+# You can think of it like a spreadsheet or SQL table.
+# It is generally the most commonly used pandas object.
+
+df = pd.DataFrame({'A': ['one', 'one', 'two', 'three'],
+                   'B': np.random.randint(1, 10, 4)})
+print(df)
+
+pt = pd.pivot_table(df, values='B', index=['A'], aggfunc='sum')
+print(pt)
