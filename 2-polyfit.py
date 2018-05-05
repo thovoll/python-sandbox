21d51e479c3910661362db60492d9a859bf28eac
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-05-04 19:57:02 -0700

    Add comments. Cleanup.

--- 2-polyfit.py
+++ 2-polyfit.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.polynomial.polynomial as poly
 import matplotlib.pyplot as plt
 
+# NumPy has a powerful N-dimensional array object (numpy.ndarray):
 x = np.linspace(0, 1, 51)
 print(x)
 
@@ -9,12 +10,11 @@ y = x**3 - x + (np.random.randn(len(x)) / 10)
 # y = x**2
 print(y)
 
+# NumPy can do least-squares fit of a polynomial to data:
 c, stats = poly.polyfit(x, y, 3, full=True)
 yt = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
 
-# c, stats = P.polyfit(x,y,2,full=True)
-# yt = c[0] + c[1]*x + c[2]*x**2
-
+# Matplotlib can easily plot:
 plt.figure(1)
 plt.subplot(211)
 plt.plot(x, y)
