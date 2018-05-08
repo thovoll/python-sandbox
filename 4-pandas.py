394862670805c9c216d9e98697921917817d5dd4
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-05-08 06:32:13 -0700

    More basic pandas.

--- 4-pandas.py
+++ 4-pandas.py
@@ -7,9 +7,55 @@ import pandas as pd
 
 df = pd.DataFrame({'A': ['one', 'one', 'two', 'three'],
                    'B': np.random.randint(1, 10, 4)})
+print("-"*10)
 print(df)
 
+# Project by label
+p = df[['A']]
+print("-"*10)
+print(p)
+print(type(p))
+
+# Select by index (single row results in Series)
+s = df.iloc[0]
+print("-"*10)
+print(s)
+print(type(s))
+
+# Select by index (multiple rows result in DataFrame)
+s = df.iloc[0:1]
+print("-"*10)
+print(s)
+print(type(s))
+
+# Select by label (single row results in Series)
+s = df.loc[0]
+print("-"*10)
+print(s)
+print(type(s))
+
+# Select by label (multiple rows result in DataFrame)
+s = df.loc[0:1]
+print("-"*10)
+print(s)
+print(type(s))
+
+# Select rows and project columns by label
+sp = df.loc[0:1, ['A']]
+print("-"*10)
+print(sp)
+print(type(sp))
+
+# Selection by criterion, then projection
+s = df[df['B'] > 4]['A']
+print("-"*10)
+print(s)
+print(type(s))
+
+# Pivot table
 pt = pd.pivot_table(df, values='B', index=['A'], aggfunc='sum')
+print("-"*10)
 print(pt)
+print(type(pt))
 
 # More: https://pandas.pydata.org/pandas-docs/stable/10min.html
