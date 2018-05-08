e58faf0fada69f9e664d083a4c8a5b43a41a1dd1
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-05-07 21:08:35 -0700

    Renumber files. Add second pandas sample.

---
+++ 5-pandas2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+housing=pd.read_csv("data/housing.csv")
+
+print(housing.head())
+print(housing.info())
+print(housing["ocean_proximity"].value_counts())
+
+housing.hist(bins=50, figsize=(20,15))
+plt.show()
